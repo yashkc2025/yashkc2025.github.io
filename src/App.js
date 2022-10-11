af0ed0885d44756b36fcb2c762b363b9c340e622
Yash Kumar <43927153+yashkc2025@users.noreply.github.com>
2022-10-12 01:07:38 +0530

    css blur

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Toolkit from './components/Toolkit';
 import tools from './components/data/tools';
+
+
 function App() {
   const tool = tools.map(item => <Toolkit {...item} />)
 
