f697aa37525c95236bda331f4e1b2bd714b969c5
Yash Kumar <43927153+yashkc2025@users.noreply.github.com>
2022-10-12 15:59:28 +0530

    added underlines

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,20 @@
-export default function Navbar(){
-    return(
-        <div className ="navbar-drop">
-        <div className="navbar">
-            <div>
-            <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Projects</a>
+export default function Navbar() {
+    return (
+        <div className="navbar-drop">
+            <div className="navbar">
+                <div>
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Projects</a>
+                </div>
+                <div>
+                    <h1>yashkc2025</h1>
+                    <h3>⤎/»</h3>
+                </div>
+                <div>
+                    <a href="#">Contact</a>
+                </div>
             </div>
-            <div>
-                <h1>yashkc2025</h1>
-                <h3>⤎/»</h3>
-            </div>
-            <div>
-            <a href="#">Contact</a>
-            </div>
-        </div>
         </div>
     )
 }
