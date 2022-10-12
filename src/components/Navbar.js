cf0c61bf723331c23320364ac16d7bb2f77544b6
Yash Kumar <43927153+yashkc2025@users.noreply.github.com>
2022-10-13 00:59:11 +0530

    media queries

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,16 +4,16 @@ export default function Navbar() {
             <div className="navbar">
                 <div>
                     {/* <a href="#">Home</a> */}
-                    <a href="#lang">About</a>
+                    <a className="about" thref="#lang">About</a>
                     <a href="#">Projects</a>
                 </div>
-                <div>
+                <div className="hiding">
                     <h1>yashkc2025</h1>
                     <h3>⤎/»</h3>
                 </div>
                 <div>
                     <a href="#">Resume</a>
-                    <a href="#contact">Say Hi  !</a>
+                    <a className="about" href="#contact">Say Hi  !</a>
                 </div>
             </div>
         </div>
