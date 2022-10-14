34c8d1e62916fdcae171e111a707e25dca1d5f10
Yash Kumar <43927153+yashkc2025@users.noreply.github.com>
2022-10-14 16:25:51 +0530

    update

--- src/components/Mobile.js
+++ src/components/Mobile.js
@@ -10,7 +10,7 @@ export default function Mobile() {
                 {/* <div className="mobile-nav-base"> */}
                     <ul id="menu">
                         <a href="#about"><li>About  ↩</li></a>
-                        <a href="#"><li>Projects ↩</li></a>
+                        <a href="#projects"><li>Projects ↩</li></a>
                         <a href="#"><li>Resume ↩</li></a>
                         <a href="#contact"><li>↪ Say Hi !</li></a>
                     </ul>
