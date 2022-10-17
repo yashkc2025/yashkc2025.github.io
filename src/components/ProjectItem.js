a9718d96de4981f25840e81d3f15ec593889929d
Yash Kumar <43927153+yashkc2025@users.noreply.github.com>
2022-10-18 01:00:31 +0530

    added more buttons

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,5 +1,17 @@
-export default function ProjectItem(props){
-    return(
+export default function ProjectItem(props) {
+    
+    let try1 = <faltu></faltu>
+    let try2 = <faltu></faltu>
+
+    if (props.trylink !== "") {
+        try1 = <button><a href={props.trylink}>Try 🡥</a></button>
+    } 
+    
+    if (props.downlink !== "") {
+        try2 = <button><a href={props.downlink}>Download ↓</a></button>
+    }
+
+    return (
         <div className="pro-item">
             <div className="pro-image-cont">
                 <img src={props.logo} className="pro-image"></img>
@@ -8,8 +20,9 @@ export default function ProjectItem(props){
                 <h1>{props.name}</h1>
                 <h3>{props.desc}</h3>
                 <button><a href={props.link}>Learn More ⤳</a></button>
+                {try1}
+                {try2}
             </div>
         </div>
-        
     )
 }
--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -3,29 +3,39 @@ export default [
         name: "Gui Calculator",
         logo: "https://repository-images.githubusercontent.com/534926619/2310d639-8977-4337-83ac-8f96d61c49b2",
         desc: `A graphical calculator app to perform mathematical functions. This app has been designed with Flutter and Dart.`,
-        link: "https://github.com/yashkc2025/gui_calculator"
+        link: "https://github.com/yashkc2025/gui_calculator",
+        trylink: "https://yashkc2025.github.io/gui_calculator/",
+        downlink: "https://github.com/yashkc2025/gui_calculator/releases/download/add_func/Calculator.apk",
     },
     {
         name: "Login Screen",
         logo: "https://repository-images.githubusercontent.com/534338049/93a8ed37-e1dc-48d6-83c2-4c8939a4daa3",
         desc: "A pink coloured login screen made using Flutter and Dart. It provides the ability to login as well as registering a user.",
-        link: "https://github.com/yashkc2025/login-grey"
+        link: "https://github.com/yashkc2025/login-grey",
+        trylink : "",
+        downlink: "",
     },
     {
         name: "Meme Generator",
         logo: "https://repository-images.githubusercontent.com/549565410/6d4223cc-f6c5-4df5-bfc0-362df8cb146b",
         desc: "A meme generator that let's you design your own memes. Just enter text and click generate meme.",
-        link: "https://github.com/yashkc2025/meme-gen"
+        link: "https://github.com/yashkc2025/meme-gen",
+        trylink : "",
+        downlink: "",
     },
     {
         name: "BMI-app",
         logo: "https://img.freepik.com/premium-vector/body-mass-index-weight-control-with-bmi-gauge_664444-1.jpg?w=2000",
         desc: "Wanna check your BMI ? This will soon become part of the calculator app.",
-        link: "https://github.com/yashkc2025/bmi-app"
+        link: "https://github.com/yashkc2025/bmi-app",
+        trylink: "https://yashkc2025.github.io/bmi-app/",
+        downlink: "https://github.com/yashkc2025/bmi-app/releases/download/1/bmi_app.1.apk",
     },
     {
         name: "Travel Journal",
         logo: "https://thumbs.dreamstime.com/b/diary-to-record-travel-vector-illustration-open-40882660.jpg",
-        desc: "A Travel journal for you to write all your memories. Made with React JS "
+        desc: "A Travel journal for you to write all your memories. Made with React JS ",
+        trylink : "",
+        downlink: "",
     }
 ]
